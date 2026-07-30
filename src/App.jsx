12389ba853ae149ed84a4780a45d26eb3a47ef01
SumeeshSunil <sumeeshsunil111@gmail.com>
2026-07-30 11:05:39 +0530

    Fixed_GitHub_Pages_SPA_routing_and_basename

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { BrowserRouter } from "react-router-dom"
-
-import AppRoutes from "./routes/AppRoutes"
-
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
-  )
+    return (
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
+            <AppRoutes />
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
